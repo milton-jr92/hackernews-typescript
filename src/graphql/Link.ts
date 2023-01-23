92b97e787e423f85b6628968be90f6ad087eab51
Milton Soares de Oliveira Junior <milton.oliveira@thoughtworks.com>
2023-01-23 18:18:38 -0300

    feat: create mutation type to add new post

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { extendType, nonNull, objectType, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";  
 
 export const Link = objectType({
@@ -30,6 +30,30 @@ export const LinkQuery = extendType({  // 2
             type: "Link",
             resolve(parent, args, context, info) {    // 4
                 return links;
+            },
+        });
+    },
+});
+
+export const LinkMutation = extendType({  // 1
+    type: "Mutation",    
+    definition(t) {
+        t.nonNull.field("post", {  // 2
+            type: "Link",  
+            args: {   // 3
+                description: nonNull(stringArg()),
+                url: nonNull(stringArg()),
+            },            
+            resolve(parent, args, context) {    
+                const { description, url } = args;  // 4                
+                let idCount = links.length + 1;  // 5
+                const link = {
+                    id: idCount,
+                    description: description,
+                    url: url,
+                };
+                links.push(link);
+                return link;
             },
         });
     },
